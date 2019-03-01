cec367dd538073fbf50ea11e12014d3b5ab739d0
Akash Rawat <akash.rawat0047@gmail.com>
2019-03-01 18:07:09 +0530

    3rd commit

--- hello.py
+++ hello.py
@@ -1,2 +1,3 @@
 print("3rd try lets see how it goes uploading .py file to First-repo")
-print("lest see this time file is uploaded to the server and i've it in my local pc commiting in it and uploading it") 
+print("lest see this time file is uploaded to the server and i've it in my local pc commiting in it and uploading it")
+print("2-3 try lets see what happen") 
